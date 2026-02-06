8313dacd7807d6796298163d90de4b98009c6a45
Nedakh1307 <nedakhelifi@gmail.com>
2026-02-06 14:13:32 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
-import { createServer } from "http";
-import { env } from "./env";
 import express from "express";
-import { Server } from "socket.io"; 
 import cors from "cors";
 import cardsRoutes from "./routes/cards.routes";
 import authRoutes from "./routes/auth.routes"; 
 import decksRoutes from "./routes/decks.routes"; 
-import { authMiddleware, AuthRequest } from "./middlewares/auth.middleware"; 
 
 // Create Express app
 export const app = express();
